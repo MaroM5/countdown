ab280c1755df6df57e68d449a4edec3cbdb6921d
MaroM5 <140432382+MaroM5@users.noreply.github.com>
2024-03-21 09:50:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -36,26 +36,25 @@ function setBackgroundAndImage(endtime) {
 
     const colors = ['#30D5C8', '#c72418', '#1829c7', '#8118c7', '#c73e18', '#18a7c7', '#600e6b', '#6b0e0e', '#d13f3f', '#a83fd1', '#76999c', '#b3a33b', '#7d240b', '#0b247d', '#427d1e', '#a3364f', '#364ea3']; 
     const images = [
-        ['Day17.jpg'],
-        ['Day16.jpg'],
-        ['Day15_02.jpg', 'Day15.jpg'],
-        ['Day14.jpg'],
-        ['Day13.jpg'],
-        ['Day12.jpg'],
-        ['Day11.jpg'],
-        ['Day10.jpg'],
-        ['Day09.jpg'],
-        ['Day08.jpg'],
-        ['Day07.jpg'],
-        ['Day06.jpg'],
-        ['Day05.jpg'],
-        ['Day04.jpg'],
-        ['Day03.jpg'],
-        ['Day02.jpg'],
-        ['Day01_06.jpg', 'Day01_05.jpg', 'Day01_04.jpg', 'Day01_03.jpg', 'Day01_02.jpg', 'Day01_01.jpg']
+        'Day16.jpg',
+        'Day15_02.jpg',
+        'Day14.jpg',
+        'Day13.jpg',
+        'Day12.jpg',
+        'Day11.jpg',
+        'Day10.jpg',
+        'Day09.jpg',
+        'Day08.jpg',
+        'Day07.jpg',
+        'Day06.jpg',
+        'Day05.jpg',
+        'Day04.jpg',
+        'Day03.jpg',
+        'Day02.jpg',
+        'Day01_06.jpg', 'Day01_05.jpg', 'Day01_04.jpg', 'Day01_03.jpg', 'Day01_02.jpg', 'Day01_01.jpg'
     ]; 
     document.body.style.backgroundColor = colors[remainingDays % colors.length]; 
-    document.getElementById('daily-image').src = images[remainingDays % images.length][0];
+    document.getElementById('daily-image').src = images[remainingDays - 1]; // Subtract 1 to account for zero-indexing
 }
 
 function initialize() {
