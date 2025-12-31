d2bbef8a6acb361f8e37c37a358e33ebb8db8069
Maro <140432382+MaroM5@users.noreply.github.com>
2025-12-31 23:41:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -32,6 +32,6 @@ function initializeClock(endtime) {
     });
 }
 
-const deadline = new Date("December 19, 2025 17:30:00 GMT+0300"); 
+const deadline = new Date("February 05, 2026 22:30:00 GMT+0300"); 
 //new Date format("Month XX, XXXX XX:XX:XX GMT+0300")
 initializeClock(deadline);
